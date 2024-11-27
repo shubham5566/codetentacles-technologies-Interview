3048c0c5c840a406ea68e58ddeffd4cdb93d8d35
shubham bhavsar <shubhambhavsar5566@gmail.com>
2024-11-27 10:45:26 +0530

    Basic setup is done

---
+++ frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import store from './app/Store.js'
+import {Provider} from 'react-redux'
+
+createRoot(document.getElementById('root')).render(
+  
+  <Provider store={store}>
+
+
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  </Provider>
+  
+
+)
